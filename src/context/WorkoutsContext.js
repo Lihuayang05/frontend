22a0fb469e4ffad289f1a211f544b47da5fed12e
Lihuayang05 <23013744@myrp.edu.sg>
2025-01-18 13:16:11 +0800

    Update WorkoutsContext.js

--- src/context/WorkoutsContext.js
+++ src/context/WorkoutsContext.js
@@ -6,15 +6,15 @@ export const workoutsReducer = (state, action) => {
   switch (action.type) {
     case 'SET_WORKOUTS':
       return { 
-        workouts: action.payload 
+        workouts: Array.isArray(action.payload) ? action.payload : []  // Ensure it's always an array
       }
     case 'CREATE_WORKOUT':
       return { 
-        workouts: [action.payload, ...state.workouts] 
+        workouts: [action.payload, ...state.workouts] // Add the new workout to the list
       }
     case 'DELETE_WORKOUT':
       return { 
-        workouts: state.workouts.filter(w => w._id !== action.payload._id) 
+        workouts: state.workouts.filter(w => w._id !== action.payload._id) // Delete the workout
       }
     default:
       return state
@@ -23,12 +23,12 @@ export const workoutsReducer = (state, action) => {
 
 export const WorkoutsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(workoutsReducer, { 
-    workouts: null
+    workouts: [] // Default to an empty array instead of null
   })
   
   return (
     <WorkoutsContext.Provider value={{ ...state, dispatch }}>
-      { children }
+      {children}
     </WorkoutsContext.Provider>
   )
 }
