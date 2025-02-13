8785d05c5d33a1b23df0d93f393dc84bb1eef0b0
Lihuayang05 <23013744@myrp.edu.sg>
2025-02-13 11:18:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // Pages & Components
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       {/* Router to handle routing */}
@@ -14,13 +19,21 @@ function App() {
         <div className="pages">
           {/* Define app routes */}
           <Routes>
-            <Route 
-              path="/" 
-              element={<Home />} 
+            {/* Home route - accessible only if user is logged in */}
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
             />
-            {/* Add more routes here if needed */}
-            {/* Example: */}
-            {/* <Route path="/about" element={<About />} /> */}
+            {/* Login route - accessible only if user is NOT logged in */}
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            {/* Signup route - accessible only if user is NOT logged in */}
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </Router>
